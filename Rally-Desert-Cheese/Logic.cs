c146994f467fb8b8bcc261e536f685fb957ab1c7
SceneMax3D <67316703+scenemax3d@users.noreply.github.com>
2020-09-24 23:54:35 +0300

    rally desert cheese initial game

---
+++ Rally-Desert-Cheese/Logic.cs
@@ -0,0 +1,12 @@
+
+public class Logic {
+
+    public SceneMax3dHostInterface.ISceneMax3DApp app;
+    static string[] numWords = {"one","two","three","four","five","six","seven","eight","nine","ten"};    
+
+
+		public string Number2Word(int num) {
+	    return numWords[num];
+    }
+    
+}
